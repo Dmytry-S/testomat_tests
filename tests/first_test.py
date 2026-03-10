ca32f46625e9daaeba302a444765b033b8b7dec7
Dmytry-S <sviridoffd@gmail.com>
2026-03-10 22:13:50 +0200

    Add test login  with invalid creds

---
+++ tests/first_test.py
@@ -0,0 +1,16 @@
+from playwright.sync_api import Page, expect
+
+
+def test_login_with_invalid_creds(page:  Page):
+    page.goto("https://testomat.io")
+
+    expect(page).to_have_title("AI Test Management Tool | Testomat.io")
+    expect(page.locator("[href*='sign_in'].login-item")).to_be_visible()
+
+    page.get_by_text("Log in", exact=True).click()
+    page.locator("#content-desktop #user_email").fill("test@gmail.co")
+    page.locator("#content-desktop #user_password").fill("12df34")
+    page.get_by_role("button", name="Sign in").click()
+
+    expect(page.locator("#content-desktop").get_by_text("Invalid Email or password.")).to_be_visible()
+    expect(page.locator("#content-desktop .common-flash-info")).to_have_text("Invalid Email or password.")
