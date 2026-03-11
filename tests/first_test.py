75e9e2b530eea4313590a51df1a8a8afd6f209f1
Dmytry-S <sviridoffd@gmail.com>
2026-03-11 21:36:32 +0200

    Add test Invalid sugn up

--- tests/first_test.py
+++ tests/first_test.py
@@ -15,3 +15,18 @@ def test_login_with_invalid_creds(page:  Page):
     expect(page.locator("#content-desktop").get_by_text("Invalid Email or password.")).to_be_visible()
     expect(page.locator("#content-desktop .common-flash-info")).to_have_text("Invalid Email or password.")
     expect(page.locator("#content-desktop").get_by_text("Forgot your password?")).to_be_enabled()
+
+def test_signup_with_invalid_creds(page: Page):
+    page.goto("https://testomat.io")
+
+    page.locator(".side-menu .start-item").click()
+    page.locator("#content-desktop #user_name").fill("Joe")
+    page.locator("#content-desktop #user_email").fill("test@gmail.co")
+    page.locator("#content-desktop #user_password").fill("147258369AS@Op")
+    page.locator("#content-desktop #user_password_confirmation").fill("147258369AS@O")
+    page.locator("#content-desktop #terms").check()
+    page.wait_for_timeout(5000)
+    page.get_by_role("button", name="Sign up").click()
+
+    expect(page.locator("#content-desktop .text-red-500")).to_have_text("doesn't match Password")
+
