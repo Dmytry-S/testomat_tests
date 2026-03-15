8fe23361a6223381ea7031926f54ccda18188bda
Dmytry-S <sviridoffd@gmail.com>
2026-03-15 16:24:19 +0200

    manuf_m_2 add dotenv library

--- tests/first_test.py
+++ tests/first_test.py
@@ -1,6 +1,10 @@
+import os
 import re
 
+from dotenv import load_dotenv
 from playwright.sync_api import Page, expect
+
+load_dotenv()
 
 
 def test_login_with_invalid_creds(page:  Page):
@@ -10,7 +14,7 @@ def test_login_with_invalid_creds(page:  Page):
     expect(page.locator("[href*='sign_in'].login-item")).to_be_visible()
 
     page.get_by_text("Log in", exact=True).click()
-    login_user(page,"dm.sv.qt@gmail.com", "2578#341")
+    login_user(page, os.getenv('INVALID_EMAIL'), os.getenv('INVALID_PASSWORD'))
 
     expect(page.locator("#content-desktop").get_by_text("Invalid Email or password.")).to_be_visible()
     expect(page.locator("#content-desktop .common-flash-info")).to_have_text("Invalid Email or password.")
@@ -18,21 +22,21 @@ def test_login_with_invalid_creds(page:  Page):
 
 def test_signup_with_invalid_creds(page: Page):
     open_home_page(page)
-    sign_up(page, "Joe", "test@gmail.co", "1472583")
+    sign_up(page, os.getenv('NAME'), os.getenv('INVALID_EMAIL'), os.getenv('INVALID_PASSWORD'))
 
     expect(page.locator("#content-desktop .text-red-500")).to_have_text(re.compile("^ must contain at least"))
 
 def test_search_project_in_company(page: Page):
-    page.goto("https://app.testomat.io/users/sign_in")
-    login_user(page,"dm.sv.qt@gmail.com", "2578#341As90KL")
+    page.goto(os.getenv('LOGIN_PAGE'))
+    login_user(page,os.getenv('EMAIL'), os.getenv('PASSWORD'))
     target_project = "Course PW Python"
     search_for_project(page, target_project)
 
     expect(page.get_by_role("heading", name=target_project)).to_be_visible()
 
 def test_should_be_possible_to_open_free_project(page: Page):
-    page.goto("https://app.testomat.io/users/sign_in")
-    login_user(page,"dm.sv.qt@gmail.com", "2578#341As90KL")
+    page.goto(os.getenv('LOGIN_PAGE'))
+    login_user(page,os.getenv('EMAIL'), os.getenv('PASSWORD'))
     select_free_project(page)
     target_project = "Course PW Python"
     search_for_project(page, target_project)
@@ -41,21 +45,21 @@ def test_should_be_possible_to_open_free_project(page: Page):
     expect(page.get_by_text("You have not created any projects yet")).to_be_visible(timeout=6000)
 
 def test_button_free_plan_should_be_visible(page: Page):
-    page.goto("https://app.testomat.io/users/sign_in")
-    login_user(page,"dm.sv.qt@gmail.com", "2578#341As90KL")
+    page.goto(os.getenv('LOGIN_PAGE'))
+    login_user(page,os.getenv('EMAIL'), os.getenv('PASSWORD'))
 
     expect(page.locator("#content-desktop .tooltip-project-plan")).to_be_visible()
 
 def test_link_read_docs_is_enabled(page: Page):
-    page.goto("https://app.testomat.io/users/sign_in")
-    login_user(page,"dm.sv.qt@gmail.com", "2578#341As90KL")
+    page.goto(os.getenv('LOGIN_PAGE'))
+    login_user(page,os.getenv('EMAIL'), os.getenv('PASSWORD'))
     select_free_project(page)
 
     expect(page.locator("[href='https://docs.testomat.io'].underline")).to_be_enabled()
 
 
 def open_home_page(page: Page):
-    page.goto("https://testomat.io")
+    page.goto(os.getenv('BASE_URL'))
 
 def sign_up(page: Page, name: str, email: str, password: str):
     page.locator(".side-menu .start-item").click()
@@ -64,7 +68,7 @@ def sign_up(page: Page, name: str, email: str, password: str):
     page.locator("#content-desktop #user_password").fill(password)
     page.locator("#content-desktop #user_password_confirmation").fill(password)
     page.locator("#content-desktop #terms").check()
-    page.wait_for_timeout(5000)
+    page.wait_for_timeout(3000)
     page.get_by_role("button", name="Sign up").click()
 
 def login_user(page: Page, email: str, password: str):
